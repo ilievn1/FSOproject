9829a363d999f287d6c399b30fc293bd3830838c
Nikolay Iliev <nikolay.iliev@aalto.fi>
2023-08-17 16:54:23 +0300

    Update associations

--- server/models/index.ts
+++ server/models/index.ts
@@ -2,6 +2,7 @@ const Vehicle = require('./vehicle');
 const Customer = require('./customer');
 const Reservation = require('./reservation');
 const Inquery = require('./inquery');
+const Feedback = require('./feedback');
 
 Customer.hasMany(Reservation);
 Reservation.belongsTo(Customer);
@@ -11,8 +12,15 @@ Vehicle.hasOne(Reservation, {
   }
 });
 Reservation.belongsTo(Vehicle);
+Reservation.hasOne(Feedback, {
+  foreignKey: {
+    allowNull: false // Cannot have feedback w/o reservation_id
+  }
+});
+Feedback.belongsTo(Reservation);
+
 module.exports = {
-  Vehicle, Customer, Reservation, Inquery
+  Vehicle, Customer, Reservation, Inquery, Feedback
 };
 
 export { };
