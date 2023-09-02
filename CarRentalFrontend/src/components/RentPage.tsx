9a42ce6154ff6b2faf3155fb2bcce3943c9e711f
Nikolay Iliev <nikolay.iliev@aalto.fi>
2023-09-02 14:21:19 +0300

    Separate dates manual logic implement

--- CarRentalFrontend/src/components/RentPage.tsx
+++ CarRentalFrontend/src/components/RentPage.tsx
@@ -1,22 +1,19 @@
 import { useLocation, useNavigate, useSearchParams } from 'react-router-dom';
 import Breadcrumbs from "./Breadcrumbs";
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
-import { Customer, DateRange } from '../types';
-import { SyntheticEvent, useMemo, useRef, useState } from 'react';
+import { Customer } from '../types';
+import { SyntheticEvent, useMemo, useRef } from 'react';
 import vehicleService from '../services/vehicle'
 import reservationService from '../services/reservation'
 import locationService from '../services/location'
-import DateRangePicker from '@wojtekmaj/react-daterange-picker'
-import '@wojtekmaj/react-daterange-picker/dist/DateRangePicker.css';
-import 'react-calendar/dist/Calendar.css';
-const TODAY = new Date()
+const TODAY = new Date().toISOString().split('T')[0];
 
 const RentPage = () => {
 
     const navigate = useNavigate();
     const [searchParams] = useSearchParams();
     const { pathname } = useLocation();
-    const [dateRange, setDateRange] = useState<DateRange>([new Date(), new Date()]);
+
     const queryClient = useQueryClient();
 
     const [brand, model, year] = useMemo(() => {
@@ -31,6 +28,8 @@ const RentPage = () => {
     const rentVehicleQuery = useQuery({ queryKey: ['rentVehicle'], queryFn: () => vehicleService.getRentVehicle(brand!, model!, year!) })
     const locationsQuery = useQuery(['locations'], locationService.getAll)
 
+    const rentDateRef = useRef<HTMLInputElement>(null);
+    const returnDateRef = useRef<HTMLInputElement>(null);
     const pickUpLocationRef = useRef<HTMLSelectElement>(null);
     const dropOffLocationRef = useRef<HTMLSelectElement>(null);
 
@@ -54,10 +53,90 @@ const RentPage = () => {
         return (<p>Error fetching locations...</p>)
     }
 
+    const setMinAndMaxDates = () => {
+        if (rentDateRef.current && returnDateRef.current) {
+            if (rentDateRef.current.value === "" && returnDateRef.current.value === "") {
+                console.log('#1111')
+                rentDateRef.current.min = TODAY;
+                rentDateRef.current.max = "";
+                returnDateRef.current.min = TODAY;
+                returnDateRef.current.max = "";
+
+            } else if (rentDateRef.current.value !== "" && returnDateRef.current.value !== "") {
+                console.log('#2222')
+
+                // Calculate the maximum return date (1 month from the rent date)
+                const chosenRentDate = new Date(rentDateRef.current.value);
+                console.log('🆁🅴🅽🆃 chosenRentDate', chosenRentDate.toISOString().split('T')[0])
+                const maxReturnDate = new Date(chosenRentDate);
+                maxReturnDate.setMonth(chosenRentDate.getMonth() + 1)
+                console.log('🆁🅴🅽🆃 maxReturnDate', maxReturnDate.toISOString().split('T')[0])
+
+                rentDateRef.current.min = TODAY;
+                console.log('🆁🅴🅽🆃 set rentDateRef.current.min', TODAY)
+                rentDateRef.current.max = returnDateRef.current.value;
+                console.log('🆁🅴🅽🆃 set rentDateRef.current.max', returnDateRef.current.value)
+                returnDateRef.current.min = chosenRentDate.toISOString().split('T')[0];
+                console.log('🆁🅴🅽🆃 set returnDateRef.current.min', chosenRentDate.toISOString().split('T')[0])
+
+                returnDateRef.current.max = maxReturnDate.toISOString().split('T')[0];
+                console.log('🆁🅴🅽🆃 set returnDateRef.current.max', maxReturnDate.toISOString().split('T')[0])
+
+            } else if (rentDateRef.current.value) {
+                console.log('#3333')
+
+                // Calculate the maximum return date (1 month from the rent date)
+                const chosenRentDate = new Date(rentDateRef.current.value);
+                console.log('🆁🅴🅽🆃 chosenRentDate', chosenRentDate.toISOString().split('T')[0])
+                const maxReturnDate = new Date(chosenRentDate);
+                maxReturnDate.setMonth(chosenRentDate.getMonth() + 1)
+                console.log('🆁🅴🅽🆃 maxReturnDate', maxReturnDate.toISOString().split('T')[0])
+
+                rentDateRef.current.min = TODAY;
+                console.log('🆁🅴🅽🆃 set rentDateRef.current.min', TODAY)
+                rentDateRef.current.max = "";
+                console.log('🆁🅴🅽🆃 set rentDateRef.current.max', "")
+                returnDateRef.current.min = chosenRentDate.toISOString().split('T')[0];
+                console.log('🆁🅴🅽🆃 set returnDateRef.current.min', chosenRentDate.toISOString().split('T')[0])
+
+                returnDateRef.current.max = maxReturnDate.toISOString().split('T')[0];
+                console.log('🆁🅴🅽🆃 set returnDateRef.current.max', maxReturnDate.toISOString().split('T')[0])
+
+
+            } else if (returnDateRef.current.value) {
+                console.log('#4444')
+
+                // Calculate the maximum return date (1 month from the rent date)
+                const maxReturnDate = new Date(returnDateRef.current.value);
+                console.log('🆁🅴🆃🆄🆁🅽 maxReturnDate', maxReturnDate)
+                const minRentDate = new Date(maxReturnDate);
+                minRentDate.setMonth(maxReturnDate.getMonth() - 1)
+                console.log('🆁🅴🆃🆄🆁🅽 minRentDate', minRentDate)
+
+                if (new Date() > minRentDate) {
+                    rentDateRef.current.min = TODAY;
+                    console.log(' 🆁🅴🆃🆄🆁🅽 rentDate min if', TODAY)
+
+                } else {
+                    rentDateRef.current.min = minRentDate.toISOString().split('T')[0];
+                    console.log(' 🆁🅴🆃🆄🆁🅽 rentDate min else', minRentDate.toISOString().split('T')[0])
+
+                }
+                rentDateRef.current.max = maxReturnDate.toISOString().split('T')[0];
+                console.log(' 🆁🅴🆃🆄🆁🅽 rentDateRef max', maxReturnDate.toISOString().split('T')[0])
+
+                // returnDateRef.current.min = rentDateRef.current.min
+                // returnDateRef.current.max = maxReturnDate.toISOString().split('T')[0];
+                returnDateRef.current.min = TODAY
+                returnDateRef.current.max = ""
+                console.log(' 🆁🅴🆃🆄🆁🅽 returnDateRef min', TODAY)
+                console.log(' 🆁🅴🆃🆄🆁🅽 returnDateRef max', "")
+            }
+        }
+    };
+
     const handleSubmit = async (event: SyntheticEvent) => {
         event.preventDefault()
-        console.log(dateRange?.toLocaleString().split(',')[0].split(' ')[0]);
-        console.log(dateRange?.toLocaleString().split(',')[1].split(' ')[0]);
 
         const customer: Customer = queryClient.getQueryData(['customer'])!
         const vid = rentVehicleQuery.data?.id
@@ -75,10 +154,8 @@ const RentPage = () => {
                 <form onSubmit={handleSubmit} className="p-6 shadow-xl rounded-lg basis-2/3">
                     <h1 className="text-2xl font-semibold mb-4">Fill out rent form</h1>
 
-                    <div>
-                        <label htmlFor='dateRange'>Select rent and return dates: </label>
-                        <DateRangePicker id='dateRange' onChange={setDateRange} value={dateRange} minDate={TODAY} required={true} defaultActiveStartDate={TODAY} autoFocus={true} />
-                    </div>
+                    <label htmlFor="rentDateInput">Rent date:</label>
+                    <input type="date" id="rentDateInput" min={TODAY} ref={rentDateRef} onChange={setMinAndMaxDates} required={true} />
 
                     <div className="mb-4 form-control w-full">
                         <label className="block text-sm font-medium mb-1">Select pick-up location</label>
@@ -91,6 +168,10 @@ const RentPage = () => {
                             })}
                         </select>
                     </div>
+
+
+                    <label htmlFor="returnDateInput">Return date:</label>
+                    <input type="date" id="returnDateInput" min={TODAY} ref={returnDateRef} onChange={setMinAndMaxDates} required={true} />
 
                     <div className="mb-4 form-control w-full">
                         <label className="block text-sm font-medium mb-1">Select drop-off location</label>
